ce511d3f56e4a057dce356c1853c2e55716f5268
Mariana Tello <mtello855@gmail.com>
2020-12-10 21:35:12 -0300

    Funcionamiento de boton play y carga de informacion de card

--- JS/section.js
+++ JS/section.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let datapi;
 const card = document.getElementById("carouselExampleControls");
 const audio = document.getElementById("audio-id");
@@ -6,30 +8,51 @@ setTimeout(function () {
   datapi = globalSpotify.dataApi;
   console.log(datapi);
 
-  card.innerHTML += `
-  <div class="carousel-inner">
-            <div class="carousel-item active">
-              <div class="card card-position" style="width: 20rem">
-                <img
-                  id="img-card"
-                  src="${datapi.data[0].artist.picture_big}"
-                  class="card-img-top"
-                  alt="..."
-                />
-                <div class="card-body">
-                  <h1 class="card-title">${datapi.data[0].artist.name}</h1>
-                  <p class="card-text">${datapi.data[0].album.title}</p>
-                  <p class="card-te">${datapi.data[0].title}</p>
-                  <div id="section-play" class="iconfont play-pause icon-play"></div>
-                 
+  card.innerHTML = `<div class="carousel-inner">
+    `;
+  datapi.data.forEach((e, index) => {
+    card.innerHTML += `
+    
+              <div class="carousel-item ${index === 0 ? "active" : ""}">
+                <div class="card card-position" style="width: 20rem">
+                  <img
+                    id="img-card"
+                    src="${e.artist.picture_big}"
+                    class="card-img-top"
+                    alt="..."
+                  />
+                  <div class="card-body">
+                    <h1 class="card-title">${e.artist.name}</h1>
+                    <p class="card-text">${e.album.title}</p>
+                    <p class="card-te">${e.title}</p>
+                    <div id="section-play${index}" class="iconfont play-pause icon-play"></div>
+                   
+                  </div>
                 </div>
-              </div>
-            </div>
-  `;
-  document
-    .getElementById("section-play")
-    .addEventListener("click", function () {
-      document.getElementById("audio-id").src = datapi.data[0].preview;
-      audio.play();
-    });
+              
+    `;
+  });
+  card.innerHTML += `</div>`;
 }, 1000);
+
+/*Se realizó otro setTimeout para que la siguiente función cargue después de la anterior.
+Se aplica un for para recorrer el largo del elemento seleccionado de datapi.
+Se crea una constante para que cada botón de play de cada card tenga su evento de click.
+Se valida que el botón esté pausado para agregar y reproducir la canción, si no es así no se agrega ni reproduce. */
+setTimeout(function () {
+  for (let i = 0; i < datapi.data.length; i++) {
+    const idPlay = `section-play${i}`;
+    document.getElementById(idPlay).addEventListener("click", function () {
+      if (document.getElementById("audio-id").paused) {
+        document.getElementById("audio-id").src = datapi.data[i].preview;
+        audio.play();
+        $(".play-pause").removeClass("icon-play");
+        $(".play-pause").addClass("icon-stop");
+      } else {
+        audio.pause();
+        $(".play-pause").removeClass("icon-stop");
+        $(".play-pause").addClass("icon-play");
+      }
+    });
+  }
+}, 3000);
