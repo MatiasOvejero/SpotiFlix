ce39d84aaede8ab7ad49ceb9a66dde7c5d19ae78
Marcelina <acostamarcelina392@gmail.com>
2020-12-29 11:58:32 -0300

    wip lista del contenido de la api

--- JS/app.js
+++ JS/app.js
@@ -2,6 +2,26 @@ let globalSpotify = {};
 let array = [];
 const inputSearch = document.getElementById("inputSearch");
 const butonSearch = document.getElementById("butonSearch");
+const favbuton = document.getElementById("favbuton");
+const reply = document.getElementById("reply");
+const carouselExampleControls = document.getElementById(
+  "carouselExampleControls"
+);
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+favbuton.addEventListener("click", function () {
+  if (carouselExampleControls.style.display === "none") {
+    carouselExampleControls.style.display === "block";
+    prev.style.display === "block";
+    next.style.display === "block";
+    reply.style.display === "none";
+  } else {
+    carouselExampleControls.style.display === "none";
+    prev.style.display === "none";
+    next.style.display === "none";
+  }
+});
 
 function apiSearch(parameter) {
   let url = `https://deezerdevs-deezer.p.rapidapi.com/search?q=${parameter}`;
@@ -15,61 +35,85 @@ function apiSearch(parameter) {
     .then((response) => response.json())
     .then((data) => {
       globalSpotify.dataApi = data;
-      let reply = document.getElementById("reply");
-      reply.innerHTML = `nombre: ${data.artist.name}`;
       console.log(data);
+      function apiSearch(parameter) {
+        for (let i = 0; i < data.data.length; i++) {
+          reply.innerHTML += ` <ul id="reply_ul">
+                <li id="reply_li"> <img
+                src="${data.data[i].artist.picture_medium}"
+                alt="..."
+                id="reply_img"/>
+                <div id= "reply_name">
+              <b id="reply_li_title">${data.data[i].title}</b> <br>
+              <h3 id="reply_li_h3">${data.data[i].artist.name}
+              - Album: ${data.data[i].album.title}</h3>
+             <button id="reply_li_button" <span class="fas fa-play"></span>Reproducir</button>
+</li>
+</ul>
+              </ul>
+                 `;
+
+          // funcion mocky
+
+          // function apiSearch(parameter) {
+          //   let url = `https://run.mocky.io/v3/eb3555e8-3bf4-44cd-b124-63a9d75f0b7f`;
+          //   fetch(url, {
+          //     method: "GET",
+          //     headers: {
+          //       headerkey: "valuerandom",
+          //     },
+          //   })
+          //     .then((response) => response.json())
+          //     .then((data) => {
+          //       globalSpotify.dataApi = data;
+          //       console.log(data);
+
+          //       for (let i = 0; i < data.data.length; i++) {
+          //         reply.innerHTML += ` <ul id="reply_ul">
+          //                    <li id="reply_li"> <img
+          //                    src="${data.data[i].artist.picture_medium}"
+          //                    alt="..."
+          //                    id="reply_img"/>
+          //                    <div id= "reply_name">
+          //                  <b id="reply_li_title">${data.data[i].title}</b> <br>
+          //                  <h3 id="reply_li_h3">${data.data[i].artist.name}
+          //                  - Album: ${data.data[i].album.title}</h3>
+          //                  </div>
+          //                  <button id="reply_li_button" <span class="fas fa-play"></span>Reproducir</button>
+          //                  </li>
+          //                 </ul>
+
+          //                    `;
+          //       }
+          //     });
+        }
+
+        //     });
+        // }
+
+        // carousel
+        inputSearch.addEventListener("keyup", function (event) {
+          if (event.keyCode === 13) {
+            event.preventDefault();
+            butonSearch.click();
+          }
+        });
+
+        butonSearch.addEventListener("click", function () {
+          const input = document.getElementById("inputSearch").value;
+          let correctInput = input.split(" ").join("%20");
+          apiSearch(correctInput);
+        });
+
+        apiSearch("Luis%20Fonsi");
+
+        sidebar;
+        $(".sidebar-list-main").click(function () {
+          $("nav ul .sidebar-list-secondary").toggleClass("secondary");
+        });
+        $(".sidebar-list-playlist").click(function () {
+          $("nav ul .sidebar-list-tertiary").toggleClass("tertiary");
+        });
+      }
     });
 }
-
-/*function mockSearch(parameter){
-  let url =`http://demo5120416.mockable.io/`;
-  fetch(url,{
-    method:"GET",
-    headers: {
-      "headerkey": "valuerandom";
-    },
-  })
-  .then((response)=> response.json())
-  .then((data) =>{
-    globalSpotify.dataApi = data;
-    console.log(data);
-  });
-} */
-
-inputSearch.addEventListener("keyup", function (event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    butonSearch.click();
-  }
-});
-
-butonSearch.addEventListener("click", function () {
-  const input = document.getElementById("inputSearch").value;
-  let correctInput = input.split(" ").join("%20");
-  apiSearch(correctInput);
-});
-
-apiSearch("Luis%20Fonsi");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// sidebar
-$(".sidebar-list-main").click(function () {
-  $("nav ul .sidebar-list-secondary").toggleClass("secondary");
-});
-$(".sidebar-list-playlist").click(function () {
-  $("nav ul .sidebar-list-tertiary").toggleClass("tertiary");
-});
